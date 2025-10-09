22acd914cee47cf4fea83cdd7953d242b38abfce
Samuel Monson <smonson@redhat.com>
2025-10-09 10:52:56 -0400

    Add a script to generate pylock from scratch
    Signed-off-by: Samuel Monson <smonson@redhat.com>

---
+++ scripts/generate_pylock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+set -e
+
+# Script to generate pylock.toml from scratch
+# If pylock.toml already exists just run `pdm lock --update-reuse`
+
+# Check if pdm is available
+if ! command -v pdm >/dev/null 2>&1
+then
+    echo "This script requires 'pdm' but it's not installed."
+    exit 1
+fi
+
+# Locking all dependencies to the same version for all supported
+# python versions is not possible (mostly due to numpy)
+# so we need to lock separately for python >=3.12 and <3.12
+pdm lock --python "~=3.10" --update-reuse
+pdm lock --append --python "<3.10" --update-reuse
