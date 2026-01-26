ab89db57d20c52335315a61278365c21360ad85d
Samuel Monson <smonson@redhat.com>
2026-01-26 14:39:00 -0500

    Ignore args on backend failure
    Signed-off-by: Samuel Monson <smonson@redhat.com>

--- src/guidellm/benchmark/schemas/generative/accumulator.py
+++ src/guidellm/benchmark/schemas/generative/accumulator.py
@@ -722,9 +722,9 @@ class GenerativeRequestsAccumulator(StandardBaseModel):
             )
 
         if response is None:
-            # FIXME: request_args is wrong
             response = GenerationResponse(
-                request_id=info.request_id, request_args=str(first_request.columns)
+                request_id=info.request_id,
+                request_args=None,
             )
 
         return response.compile_stats(
