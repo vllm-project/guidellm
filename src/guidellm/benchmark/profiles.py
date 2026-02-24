6ffc31d745cd6ded98a889e1ec81ed255e639c9d
Uri Shaket <ushaket@redhat.com>
2026-02-24 17:06:18 +0200

    Revert "add ignore[arg-type] None type is checked earlier in the flow"
    This reverts commit 2538f49d5184eb827a923d5c2090e23ddb56402c.
    
    Signed-off-by: Uri Shaket <ushaket@redhat.com>

--- src/guidellm/benchmark/profiles.py
+++ src/guidellm/benchmark/profiles.py
@@ -164,7 +164,7 @@ class Profile(
         return [strat.type_ for strat in self.completed_strategies]
 
     @staticmethod
-    def _should_stop_escalating(prev_benchmark: Benchmark | None) -> bool:
+    def _should_stop_escalating(prev_benchmark: Benchmark) -> bool:
         """
         Check if a benchmark was terminated by a failure constraint.
 
@@ -174,8 +174,7 @@ class Profile(
         Constraints that use "stop_local" (max duration, max requests) are normal
         completions and do not trigger escalation stops.
 
-        :param prev_benchmark: Benchmark instance with a scheduler_state
-            attribute, or None
+        :param prev_benchmark: Benchmark instance with a scheduler_state attribute
         :return: True if a failure constraint was triggered, False otherwise
         """
         scheduler_state = getattr(prev_benchmark, "scheduler_state", None)
