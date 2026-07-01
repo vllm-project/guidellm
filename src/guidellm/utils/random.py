d7d6865b7077257fe6a1385762e116781c1b2c81
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-07-01 15:26:06 -0400

    synthetic text dataset support for requeue delay
    Signed-off-by: SkiHatDuckie <SkiHatDuckie@gmail.com>

--- src/guidellm/utils/random.py
+++ src/guidellm/utils/random.py
@@ -1,7 +1,7 @@
 import random
 from collections.abc import Iterator
 
-__all__ = ["IntegerRangeSampler"]
+__all__ = ["FloatRangeSampler", "IntegerRangeSampler"]
 
 
 class IntegerRangeSampler:
@@ -37,6 +37,44 @@ class IntegerRangeSampler:
                 yield calc_min
             elif not self.variance:
                 yield self.rng.randint(calc_min, calc_max)
+            else:
+                rand = self.rng.gauss(self.average, self.variance)
+                yield round(max(calc_min, min(calc_max, rand)))
+
+
+class FloatRangeSampler:
+    def __init__(
+        self,
+        average: float,
+        variance: float | None,
+        min_value: float | None,
+        max_value: float | None,
+        random_seed: int,
+    ):
+        self.average = average
+        self.variance = variance
+        self.min_value = min_value
+        self.max_value = max_value
+        self.seed = random_seed
+        self.rng = random.Random(random_seed)  # noqa: S311
+
+    def __iter__(self) -> Iterator[float]:
+        calc_min = self.min_value
+        if calc_min is None:
+            calc_min = max(
+                1, self.average - 5 * self.variance if self.variance else self.average
+            )
+        calc_max = self.max_value
+        if calc_max is None:
+            calc_max = (
+                self.average + 5 * self.variance if self.variance else self.average
+            )
+
+        while True:
+            if calc_min == calc_max:
+                yield calc_min
+            elif not self.variance:
+                yield self.rng.uniform(calc_min, calc_max)
             else:
                 rand = self.rng.gauss(self.average, self.variance)
                 yield round(max(calc_min, min(calc_max, rand)))
