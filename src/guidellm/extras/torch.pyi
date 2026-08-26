7c0a5c62db358df5f860e19dc49273a449a5b6db
Samuel Monson <smonson@redhat.com>
2026-08-26 18:23:38 -0400

    Revert type interface * import
    For some reason this works for numpy but not the other packages.
    
    Signed-off-by: Samuel Monson <smonson@redhat.com>

--- src/guidellm/extras/torch.pyi
+++ src/guidellm/extras/torch.pyi
@@ -1,1 +1,3 @@
-from torch import *  # noqa: F403
+from torch import Tensor, from_numpy, int16, is_floating_point, nn, uint8
+
+__all__ = ["Tensor", "from_numpy", "int16", "is_floating_point", "nn", "uint8"]
--- src/guidellm/extras/vllm.pyi
+++ src/guidellm/extras/vllm.pyi
@@ -1,1 +1,8 @@
-from vllm import *  # noqa: F403
+from vllm import AsyncEngineArgs, AsyncLLMEngine, RequestOutput, SamplingParams
+
+__all__ = [
+    "AsyncEngineArgs",
+    "AsyncLLMEngine",
+    "RequestOutput",
+    "SamplingParams",
+]
